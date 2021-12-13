9fc4c8f4e21cd236855a38cfb341e6ffbd924882
Garikoitz11 <garikoitzsg11@gmail.com>
2021-12-13 14:56:23 +0100

    cambios

--- docker-lamp/app/datuakErregistratu.php
+++ docker-lamp/app/datuakErregistratu.php
@@ -1,41 +1,36 @@
 <?php
 
-    $hostname = "db";
-    $username = "admin";
-    $password = "test";
-    $db = "database";
-
-    $conn = new mysqli($hostname,$username,$password,$db);
-    
-    /*if ($conn->connect_error) {
-      die("Database connection failed: " . $conn->connect_error);
-    }*/
+  $conn = new mysqli('db','admin','test','database');
   
-    $conn->set_charset('utf8');
+  /*if ($conn->connect_error) {
+    die("Database connection failed: " . $conn->connect_error);
+  }*/
 
-    $Erabiltzailea=$conn->real_escape_string($_POST['erabiltzaile']);
-    $Izena=$conn->real_escape_string($_POST['izena']);
-    $Abizena=$conn->real_escape_string($_POST['abizena']);
-    $PostaKodea=$conn->real_escape_string($_POST['posta-kodea']);
-    $NAN=$conn->real_escape_string($_POST['NAN']);
-    $JaiotzaData=$conn->real_escape_string($_POST['JaiotzaData']);
-    $Pasahitza=$conn->real_escape_string($_POST['pasahitza']);
-    $PostaElektronikoa=$conn->real_escape_string($_POST['postaElektronikoa']);
-    $Mugikorra=$conn->real_escape_string($_POST['mugikorra']);
+  $conn->set_charset('utf8');
 
-    if($kontsultaBerria = $conn->prepare("INSERT INTO Erregistroa (Erabiltzailea, Izena, Abizenak, PostaKodea, NAN, JaiotzaData, Pasahitza, PostaElektronikoa, Mugikorra) VALUES (?,?,?,?,?,?,?,?,?)")){
+  $Erabiltzailea=$conn->real_escape_string($_POST['erabiltzaile']);
+  $Izena=$conn->real_escape_string($_POST['izena']);
+  $Abizena=$conn->real_escape_string($_POST['abizena']);
+  $PostaKodea=$conn->real_escape_string($_POST['posta-kodea']);
+  $NAN=$conn->real_escape_string($_POST['NAN']);
+  $JaiotzaData=$conn->real_escape_string($_POST['JaiotzaData']);
+  $Pasahitza=$conn->real_escape_string($_POST['pasahitza']);
+  $PostaElektronikoa=$conn->real_escape_string($_POST['postaElektronikoa']);
+  $Mugikorra=$conn->real_escape_string($_POST['mugikorra']);
 
-      $kontsultaBerria->bind_param('sssisissi', $Erabiltzailea, $Izena, $Abizena, $PostaKodea, $NAN, $JaiotzaData, $Pasahitza, $PostaElektronikoa, $Mugikorra);
-      $kontsultaBerria->execute();
+  if($kontsultaBerria = $conn->prepare("INSERT INTO Erregistroa (Erabiltzailea, Izena, Abizenak, PostaKodea, NAN, JaiotzaData, Pasahitza, PostaElektronikoa, Mugikorra) VALUES (?,?,?,?,?,?,?,?,?)")){
 
-      if($kontsultaBerria){
-        echo "<script>alert('Erabiltzailea erregistratu da');
-        window.location.href='index.html'</script>";
-      }
-      else{
-        echo "<script>alert('Beste erabiltzaile izen bat aukeratu'); 
-        window.history.go(-1);</script>";
-      }
-      $kontsultaBerria->close();
+    $kontsultaBerria->bind_param('sssisissi', $Erabiltzailea, $Izena, $Abizena, $PostaKodea, $NAN, $JaiotzaData, $Pasahitza, $PostaElektronikoa, $Mugikorra);
+    $kontsultaBerria->execute();
+
+    if($kontsultaBerria){
+      echo "<script>alert('Erabiltzailea erregistratu da');
+      window.location.href='index.html'</script>";
     }
+    else{
+      echo "<script>alert('Beste erabiltzaile izen bat aukeratu'); 
+      window.history.go(-1);</script>";
+    }
+    $kontsultaBerria->close();
+  }
 ?>
