1304253c8bad4e57cd4ca70fa8139a921ca96a4e
Garikoitz11 <garikoitzsg11@gmail.com>
2021-12-17 01:02:18 +0100

    cambios

--- docker-lamp/app/saioaHasi.php
+++ docker-lamp/app/saioaHasi.php
@@ -7,36 +7,42 @@
         $username = "admin";
         $password = "test";
         $db = "database";
+        $db2 = "log";
 
         session_start();
 
-        $conn = mysqli_connect($hostname,$username,$password,$db);
+        $conn = new mysqli($hostname,$username,$password,$db);
+        $conn2 = new mysqli($hostname,$username,$password,$db2);
 
         $conn->set_charset('utf8');
+        $conn2->set_charset('utf8');
 
         $Erabiltzaile = $conn->real_escape_string($_POST['erabiltzaile']);
         $Pasahitza = $conn->real_escape_string($_POST['pasahitza']);
+        $Data = date("Y-m-d");
+        $Hordu = date("H:i:s");
 
-        if($kontsultaBerria = $conn->prepare("SELECT * FROM Erregistroa WHERE Erabiltzailea = ? AND Pasahitza = ?")){
+        $kontsultaBerria = $conn->prepare("SELECT * FROM Erregistroa WHERE Erabiltzailea = ? AND Pasahitza = ?");
+        $kontsulta2 = $conn2->query("INSERT INTO Loga (erabiltzaile, dat, hordua) VALUES ('$Erabiltzaile', '$Data', '$Hordu')");
 
-            $kontsultaBerria->bind_param('ss', $Erabiltzaile, $Pasahitza);
-            $kontsultaBerria->execute();
-            $emaitza = $kontsultaBerria->get_result();
+        $kontsultaBerria->bind_param('ss', $Erabiltzaile, $Pasahitza);
+        $kontsultaBerria->execute();
 
-            if($emaitza->num_rows == 1){
-                //echo "<script>window.location.href='datuakAldatu.php'</script>";
-                $_SESSION['izena'] = $Erabiltzaile;
-                $_SESSION['denbora'] = time();
-                echo "<script>alert('Ongi etorri $Erabiltzaile');window.location.href='index.php'</script>";
-                
+        $emaitza = $kontsultaBerria->get_result();
 
-            }
-            else{
-                echo "<script>alert('Erabiltzailea ezin izan du saioa hasi');
-                window.location.href='index.php'</script>";
-            }
-            $kontsultaBerria->close();
+        if($emaitza->num_rows == 1){
+            //echo "<script>window.location.href='datuakAldatu.php'</script>";
+            $_SESSION['izena'] = $Erabiltzaile;
+            $_SESSION['denbora'] = time();
+            echo "<script>alert('Ongi etorri $Erabiltzaile');window.location.href='index.php'</script>";
+            
+
         }
+        else{
+            echo "<script>alert('Erabiltzailea ezin izan du saioa hasi');
+            window.location.href='index.php'</script>";
+        }
+        $kontsultaBerria->close();
         
         
     #}
