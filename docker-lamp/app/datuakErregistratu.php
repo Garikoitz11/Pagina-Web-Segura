616047e71e23318cf018386692130123f212bfe8
UnaiSolaun <unai.solaun@zaraobe.net>
2021-12-18 19:48:46 +0100

    Contraseñas y tal

--- docker-lamp/app/datuakErregistratu.php
+++ docker-lamp/app/datuakErregistratu.php
@@ -14,12 +14,13 @@
   $Pasahitza=$conn->real_escape_string($_POST['pasahitza']);
   $PostaElektronikoa=$conn->real_escape_string($_POST['postaElektronikoa']);
   $Mugikorra=$conn->real_escape_string($_POST['mugikorra']);
-  $Bankua=$conn->real_escape_string($_POST['datuBankarioak']); 
+  $Bankua=$conn->real_escape_string($_POST['datuBankarioak']);
 
-  $kontsultaBerria = $conn->query("INSERT INTO Erregistroa VALUES ('$Erabiltzailea', '$Izena', '$Abizena', '$PostaKodea', '$NAN', '$JaiotzaData', '$Pasahitza', '$PostaElektronikoa', '$Mugikorra', '$Bankua')");
+  $PasZifratuta=password_hash($Pasahitza,PASSWORD_BCRYPT);
+  $BankuZifratuta=base64_encode($Bankua);
 
-  //aa
-
+  $kontsultaBerria = $conn->query("INSERT INTO Erregistroa VALUES ('$Erabiltzailea', '$Izena', '$Abizena', '$PostaKodea', '$NAN', '$JaiotzaData', '$PasZifratuta', '$PostaElektronikoa', '$Mugikorra', '$BankuZifratuta')");
+  
   if($kontsultaBerria){
     $_SESSION['izena'] = $Erabiltzailea;
     $_SESSION['denbora'] = time();
