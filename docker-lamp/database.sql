0d52d1e7aabf512ca95d58cd4d6e5f590b6f132e
Garikoitz11 <garikoitzsg11@gmail.com>
2021-12-19 15:44:10 +0100

    cambios

--- docker-lamp/database.sql
+++ docker-lamp/database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: db
--- Tiempo de generación: 17-12-2021 a las 11:33:50
+-- Tiempo de generación: 19-12-2021 a las 14:39:25
 -- Versión del servidor: 10.6.4-MariaDB-1:10.6.4+maria~focal
 -- Versión de PHP: 7.4.20
 
@@ -45,9 +45,8 @@ CREATE TABLE `Erregistroa` (
 --
 
 INSERT INTO `Erregistroa` (`Erabiltzailea`, `Izena`, `Abizenak`, `PostaKodea`, `NAN`, `JaiotzaData`, `Pasahitza`, `PostaElektronikoa`, `Mugikorra`, `BankuDatuak`) VALUES
-('Gari45', 'Garikoitz', 'Salaberria', 48903, '79181000G', '2001-01-11', '1234', 'gari@gmail.com', 999888999, 'ES1234567890123456789012'),
-('janire', 'hshhs', 'hshsh', 48903, '22761980F', '2001-12-14', 'Janire123!', 'hghg@gmail.com', 666666666, 'ES1234567891234567891234'),
-('Janire12', 'Janire', 'Veganzones', 48903, '79181000G', '2001-12-14', 'Jani123?', 'janire@ehu.eus', 999888999, 'ES12');
+('Gari45', 'Garikoitz', 'Salaberria', 48903, '79181000G', '2001-01-11', '$2y$10$qroU0EjQFBjy48VYMQwuR.834OSNA/ZZvaPXidTYLLpPhoNG4vmBe', 'gari45@gmail.com', 666666999, 'RVM0MDAxMjM0NTY3ODkwMTIzNDU2Nzg5'),
+('Janire12', 'Janire', 'Veganzones', 48903, '12345678Z', '2001-12-14', '$2y$10$M06phkqfHPFUL4PDfOuFmOTOBb1KltD88h6gCixbUkQE1T4t9sgbG', 'janire@ikasle.ehu.eus', 999888999, 'RVM0MDk4NzY1NDMyMTA5ODc2NTQzMjEw');
 
 -- --------------------------------------------------------
 
@@ -68,11 +67,9 @@ CREATE TABLE `Loga` (
 --
 
 INSERT INTO `Loga` (`id`, `erabiltzaile`, `dat`, `hordua`, `saioaHasiDa`) VALUES
-(1, 'Juan', '2021-12-17', '00:56:00', 0),
-(4, 'Gari45', '2021-12-17', '11:17:40', 0),
-(9, 'kdkd', '2021-12-17', '11:29:57', 0),
-(10, 'Gari45', '2021-12-17', '11:30:10', 0),
-(11, 'Gari45', '2021-12-17', '11:30:19', 1);
+(1, 'Gari45', '2021-12-19', '14:34:26', 1),
+(2, 'Gari45', '2021-12-19', '14:34:52', 1),
+(3, 'Gari45', '2021-12-19', '14:35:05', 0);
 
 -- --------------------------------------------------------
 
@@ -87,6 +84,16 @@ CREATE TABLE `Produktuak` (
   `Deskribapena` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL,
   `Prezioa` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `Produktuak`
+--
+
+INSERT INTO `Produktuak` (`Kodea`, `Izena`, `Mota`, `Deskribapena`, `Prezioa`) VALUES
+(1, 'Sansumg Galaxy S8', 'Mugikorra', 'Mugikor oso ona eta merkea', 199.99),
+(2, 'Iphone X', 'Mugikorra', 'Mugikor ikaragarria', 700),
+(3, 'Huawei Matebook D14', 'Ordenagailua', 'Ordenagailu ona unibertsitatean erabiltzeko', 599),
+(4, 'Canon PowerShot', 'Kamara', 'Argazki ikaraggariak egiten dituen kamara ona', 299.99);
 
 --
 -- Índices para tablas volcadas
@@ -118,13 +125,13 @@ ALTER TABLE `Produktuak`
 -- AUTO_INCREMENT de la tabla `Loga`
 --
 ALTER TABLE `Loga`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `Produktuak`
 --
 ALTER TABLE `Produktuak`
-  MODIFY `Kodea` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `Kodea` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
